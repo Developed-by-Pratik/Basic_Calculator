1cf73e004c9bac1bd116f607bd7a25cca98fdb2c
Pratik Kakade <Pratikkakade4618@gmail.com>
2023-09-01 15:57:00 +0530

    All Files are Uploaded

---
+++ script.js
@@ -0,0 +1,31 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
+updateDisplay();
+
+function appendToDisplay(value) {
+    if (displayValue === '0') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
